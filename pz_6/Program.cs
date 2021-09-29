e803005c1a625e743e7878f619c0e4413c589a1b
Христина Минеева <h.mineeva@yandex.ru>
2021-09-29 13:06:59 +0300

    pz 6

---
+++ pz_6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 16 вариант
+            int x = -2;
+            int t = int.Parse(Console.ReadLine());
+            while (x <= 12)
+            {
+                if (x < 0)
+                {
+                    Console.WriteLine(t);
+                }
+                else if (x >= 10)
+                {
+                    Console.WriteLine(t * 2);
+                }    
+                else
+                {
+                    Console.WriteLine(t * x);
+                }
+                x += 2;
+            }
+            Console.ReadLine();
+        }
+    }
+}
