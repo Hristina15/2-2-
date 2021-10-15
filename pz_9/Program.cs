025d29800807f3c25894cd3c46d9c9079eb68f6a
Христина Минеева <h.mineeva@yandex.ru>
2021-10-15 12:30:15 +0300

    pz_9

---
+++ pz_9/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //16 вариант
+            int[,] A = new int[10, 10];
+            int[] S = new int[10];
+            var rand = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    A[i, j] = rand.Next(100);
+                }
+            }
+
+            //сумма элемента по столбцу
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    S[i] += A[j, i];
+                }
+            }
+
+            foreach (int B in S)
+            {
+                Console.WriteLine(B);
+            }
+            Console.ReadLine();
+        }
+    }
+}
